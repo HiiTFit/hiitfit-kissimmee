701c768b669db26f46ad83c794a644b68550fb12
Alex Soper <alexsoper29@gmail.com>
2017-11-21 18:07:02 -0500

    Black Friday Special going live

--- js/template-scripts.js
+++ js/template-scripts.js
@@ -19,11 +19,53 @@ jQuery(document).ready(function ($) {
         singleItem: true,
         transitionStyle: "fade"
     });
-   $("#shop-button").click(function() {
-      window.location.href = 'http://hiitfit.wodify.com/OnlineSalesPortal/PlansEntry.aspx'
-   });
+    $("#shop-button").click(function () {
+        window.location.href = 'http://hiitfit.wodify.com/OnlineSalesPortal/PlansEntry.aspx';
+    });
 
-    $("#free-class-button").click(function() {
-      window.location.href = 'http://hiitfit.wodify.com/OnlineSalesPortal/SessionsByLocationEntry.aspx?LocationId=4831&OnlineMembershipId=60787'
-   });
+    $("#free-class-button").click(function () {
+        window.location.href = 'http://hiitfit.wodify.com/OnlineSalesPortal/SessionsByLocationEntry.aspx?LocationId=4831&OnlineMembershipId=60787';
+    });
+
+    function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        var days = Math.floor(t / (1000 * 60 * 60 * 24));
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function initializeClock(id, endtime) {
+        var clock = document.getElementById(id);
+        var daysSpan = clock.querySelector('.days');
+        var hoursSpan = clock.querySelector('.hours');
+        var minutesSpan = clock.querySelector('.minutes');
+        var secondsSpan = clock.querySelector('.seconds');
+
+        function updateClock() {
+            var t = getTimeRemaining(endtime);
+
+            daysSpan.innerHTML = t.days;
+            hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+            minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+            secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+            if (t.total <= 0) {
+                clearInterval(timeinterval);
+            }
+        }
+
+        updateClock();
+        var timeinterval = setInterval(updateClock, 1000);
+    }
+
+    var deadline = new Date(2017, 10, 24, 6, 0, 0, 0);
+    initializeClock('clockdiv', deadline);
 });

a6ec04571156ce6b2ec0d885562d351fdd9894ef
asoper29 <alexsoper29@gmail.com>
2020-01-01 10:44:18 -0500

    Add new promo video

--- js/template-scripts.js
+++ js/template-scripts.js
@@ -1,29 +1,53 @@
 jQuery(document).ready(function ($) {
     // Owl Carousel
-    $(".carousel-default").owlCarousel({
+    $('.carousel-default').owlCarousel({
         navigation: true,
         slideSpeed: 300,
         paginationSpeed: 400,
         autoPlay: true,
         addClassActive: true,
-        navigationText: ["&#xf007", "&#xf006"],
+        navigationText: ['&#xf007', '&#xf006'],
         singleItem: true
     });
-    $(".carousel-fade-transition").owlCarousel({
+    $('.carousel-fade-transition').owlCarousel({
         navigation: true,
         slideSpeed: 300,
         paginationSpeed: 400,
         autoPlay: true,
         addClassActive: true,
-        navigationText: ["&#xf007", "&#xf006"],
+        navigationText: ['&#xf007', '&#xf006'],
         singleItem: true,
-        transitionStyle: "fade"
+        transitionStyle: 'fade'
     });
-    $("#shop-button").click(function () {
+    $('#shop-button').click(function () {
         window.location.href = 'http://hiitfit.wodify.com/OnlineSalesPortal/PlansEntry.aspx';
     });
 
-    $("#free-class-button").click(function () {
+    $('#free-class-button').click(function () {
         window.location.href = 'http://hiitfit.wodify.com/OnlineSalesPortal/SessionsByLocationEntry.aspx?LocationId=4831&OnlineMembershipId=60787';
     });
+
+    const bodyWidth = $(window).width();
+    const video = document.createElement('video');
+    const videoCredit = $('#ig-video-credit');
+    video.autoplay = video.muted = video.controls = true;
+    if (bodyWidth >= 960) {
+        video.src = 'img/hiitfit-promo-540p.mp4';
+        video.width = 960;
+        video.height = 540;
+        $('#ig-video-credit').addClass('lg-vid');
+    } else if (bodyWidth >= 640) {
+        video.src = 'img/hiitfit-promo-360p.mp4';
+        video.width = 640;
+        video.height = 360;
+        $('#ig-video-credit').addClass('md-vid');
+    } else {
+        video.src = 'img/hiitfit-promo-240p.mp4';
+        video.width = 426;
+        video.height = 240;
+        $('#ig-video-credit').addClass('sm-vid');
+    }
+
+    document.getElementById('promo-video-container').appendChild(video);
+    video.play();
 });
